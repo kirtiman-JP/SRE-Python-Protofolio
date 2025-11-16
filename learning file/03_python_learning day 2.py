867327ab19c8043ab01f3476f838c67e4cef7b04
kirtiman-JP <kirtimansingh61@gmail.com>
2025-11-16 22:19:19 +0530

    day 2.5

---
+++ learning file/03_python_learning day 2.py
@@ -0,0 +1,9 @@
+import pandas
+import hashlib
+import tensorflow
+print("Hello world", 7)
+print(5)
+print("Bye")
+print(17*13)
+print("Ok,so this is my d2 of learning python or git hub whatever. so  imay be pure beginer at everything but as i keep learn ill make sure that ill improve myself to an professional lvl so that ia am not behind of others. hope u understand and give me an chance and watch me lvl up. thanks")
+print("thats for today")
