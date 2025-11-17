20e3b14b8444586c66c9f67b72f96d383bfed025
kirtiman-JP <kirtimansingh61@gmail.com>
2025-11-17 22:42:51 +0530

    learnt to use comments and escape sequence characters. with other parameters of print statement

---
+++ learning file/04_python_learning day 3.py
@@ -0,0 +1,10 @@
+print("Hey i am a good boy\nand this person is also a good boy")
+# hello this is an character. one more thing if u tpe insert then to remove overwrite function press fn+delete.
+#author: kirtiman
+# ok so gonna try this its like if u select lines and use ctrl+/ it turns the line or code written to comment
+# print("Hello world")
+'''ok so gonaa try somthing now'''
+""" or it can be done like this also but single is ok if want to"""
+print("hello \"ABHINAV\" ")
+print("hello world", 6, 7, sep="_", end="ok bye\n" )
+print("good night")
