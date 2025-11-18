7ffaf7c8b885fc2c5637cf60b809109d5cac1223
kirtiman-JP <kirtimansingh61@gmail.com>
2025-11-18 21:01:17 +0530

    learnt variables and data types

---
+++ learning file/05_python_learning day 4.py
@@ -0,0 +1,21 @@
+a = 1.1 
+print("a")
+# Harry = 9
+# print(Harry)
+b = ("Harry")
+print(b) 
+c = True
+d = None 
+a1 = 9
+print (a+a1)
+print("The type of a is", type(a))
+print("The type of b is", type(b))
+print("The type of c is", type(c))
+print("The type of d is", type(d))
+print("The type of a1 is", type(a1))
+list1 = [8, 2, 3, [-4, 5], ["Apple", "banana"]]
+print(list1)
+tuple1 = (("Parrot", "Spparow"), ("Lion", "Tiger"))
+print(tuple1)
+dict1 = {"name":"sakshi", "age":20, "canVote":True}
+print(dict1)
