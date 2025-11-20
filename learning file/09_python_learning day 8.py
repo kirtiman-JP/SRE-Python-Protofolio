e88dbba9d1600360ba14438fd515151befc60cff
kirtiman-JP <kirtimansingh61@gmail.com>
2025-11-20 22:46:44 +0530

    project: make a program to carry out arthemetic expression using the input function

--- learning file/09_python_learning day 8.py
+++ learning file/09_python_learning day 8.py
@@ -1,3 +1,4 @@
+# user input
 a = input()
 print("your name is:", a)
 #or
---
+++ learning file/10_python_learning day 8.py
@@ -0,0 +1,9 @@
+a = float(input("Enter your first value: "))
+b = float(input("Enter your second value: "))
+
+print("The sum of:", a, "+", b, "is", a + b )
+print("The sum of:", a, "-", b, "is", a - b )
+print("The sum of:", a, "*", b, "is", a * b )
+print("The sum of:", a, "/", b, "is", a / b )
+print("Thank you for using it :)")
+
