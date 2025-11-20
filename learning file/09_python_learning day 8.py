6537234b0f247c2d41f0fa667797087a30a3aba7
kirtiman-JP <kirtimansingh61@gmail.com>
2025-11-20 21:43:17 +0530

    learnt to take input from user

---
+++ learning file/09_python_learning day 8.py
@@ -0,0 +1,11 @@
+a = input()
+print("your name is:", a)
+#or
+b = input("Enter yourname: ")
+print("your name is:", b)
+#another type.
+x = input("Enter your value: ")
+y = input("Enter your value: ")
+print( x + y)
+#see up input as string so it will join so covert them to int beffore running if we want it to do operation  
+print(int(x) + int(y))
