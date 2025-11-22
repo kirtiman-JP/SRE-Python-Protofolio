26353117423c29a7a78c1395e5e484ac45cc3638
kirtiman-JP <kirtimansingh61@gmail.com>
2025-11-22 22:39:16 +0530

    learnt various strings methods in python

---
+++ learning file/13_python_learning day 10.py
@@ -0,0 +1,59 @@
+a = "Harry"
+print(a.upper())
+b = "Harry @@@@"
+print(b.rstrip("@"))
+c = "Hary"
+print(c.replace("Hary", "kirtiman"))
+d = "Harry is tool"
+print(d.split(" "))
+e = "intro to js"
+print(e.capitalize())
+f = "welcome to my channel"
+print(f.center(78))
+print(f.center(78, "."))
+g = ("Harry", "Harry", "Kirtiman")
+print(g.count("Harry"))
+h = "kirtiman is a idiot. no right?"
+print(h.endswith("no"))
+print(h.endswith("?"))
+print(h.endswith("idiot.", 4, 10))
+print(h.endswith("is", 0, 17))
+i = "kirtiman is a idiot. no right?"
+print(i.find("kirtiman"))
+print(i.find("idiot"))
+j = "Kirtiman is an baka"
+print(j.index("is"))
+k = "i am supe"
+print(k.isalnum())
+l = "iamSupe01"
+print(l.isalnum())
+m = "ioioioioio"
+print(m.isalpha())
+n = "ioio9090"
+print(n.isalpha())
+o = " i want to end ths shit now"
+print(o.islower())
+p = "can i end this now?"
+print(p.isprintable())
+q = "can i end this now\n"
+print(q.isprintable())
+r = "wil this work"
+print(r.isspace())
+s = "     "
+print(s.isspace())
+t = "World Health Organization"
+print(t.istitle())
+u = "I do know"
+print(u.istitle())
+w = "AEIOU"
+print(w.isupper())
+x = "Python is an compiled language"
+print(x.replace("compiled", "Interpreted"))
+y = "kirtiman is a gooooood boy"
+print(y.startswith("kirtiman"))
+print(y.startswith("boy"))
+z="kirtiman Are You Oky. And Kirtiman aRE yOU oKAY"
+print(z.swapcase())
+aa= "Kirtiman tis is the final one take  care. final one for today not final say ok"
+print(aa.title())
+print(j.index("zoo"))
