3be510d24d1de7fb59520f8a9d9af133d3ba8ab1
kirtiman-JP <kirtimansingh61@gmail.com>
2025-11-23 22:35:50 +0530

    learnt if else statments

---
+++ learning file/14_python_learning day 11.py
@@ -0,0 +1,37 @@
+
+a = int(input("Enter your age: "))
+print("Your age is:", a)
+
+if(a>18):
+    print("\"yes\"")
+    print("You can drive")
+else:
+    print("\"no\"")
+    print("you cant drive")
+applePrice = 20
+budget =50
+if(applePrice<=budget):
+    print("Alexa, add Apples to my cart.")
+else:
+    print("Alexa, don't add Apples to my cart.")
+num = int(input('enetr the value of number: '))
+if(num<0):
+    print("number is negetive")
+elif(num == 0):
+    print("number is zero")
+else:
+    print("Number is positive")
+
+
+
+num = int(input("Enter your value: "))
+if(num<0):
+    print("Number is negetive.")
+elif(num>0):
+    if(num<+10):
+        print("number is between 1 - 10")
+    elif(num>10 and num<=20):
+        print("Number is between 11 - 20.")
+    else:("Print number is greater then 20.")
+else:
+    print("Number is zero")
