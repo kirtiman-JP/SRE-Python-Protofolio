3533f682ca095a55533ff8dd047c9315f6e8ae74
kirtiman-JP <kirtimansingh61@gmail.com>
2025-11-26 21:41:41 +0530

    learnt for loops

---
+++ learning file/17_python_learning day 14.py
@@ -0,0 +1,16 @@
+name = 'Kirtiman'
+for z in name:
+    print(z, end = ", ")
+    if (z == "K"):
+     print ("A name with K. rare enough hugh.")
+colours = ["Red", "Green", "Blue", "Pink"]
+for x in colours:
+    print(x)
+    for i in x:
+        print(i)
+for k in range(6):
+    print(k+1)
+for k in range(6):
+    print(k)
+for k in range(1, 200):
+    print(k)
