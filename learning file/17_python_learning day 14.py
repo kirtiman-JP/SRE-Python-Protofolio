8254bd8896369a7492f9ae6feff11af28c50ea5e
kirtiman-JP <kirtimansingh61@gmail.com>
2025-11-27 21:45:43 +0530

    learnt while loop

--- learning file/17_python_learning day 14.py
+++ learning file/17_python_learning day 14.py
@@ -1,16 +1,20 @@
-name = 'Kirtiman'
-for z in name:
-    print(z, end = ", ")
-    if (z == "K"):
-     print ("A name with K. rare enough hugh.")
-colours = ["Red", "Green", "Blue", "Pink"]
-for x in colours:
-    print(x)
-    for i in x:
-        print(i)
-for k in range(6):
-    print(k+1)
-for k in range(6):
-    print(k)
-for k in range(1, 200):
-    print(k)
+# name = 'Kirtiman'
+# for z in name:
+#     print(z, end = ", ")
+#     if (z == "K"):
+#      print ("A name with K. rare enough hugh.")
+# colours = ["Red", "Green", "Blue", "Pink"]
+# for x in colours:
+#     print(x)
+#     for i in x:
+#         print(i)
+# for k in range(6):
+#     print(k+1)
+# for k in range(6):
+#     print(k)
+# for k in range(1, 200):
+#     print(k)
+
+z = [10]
+for item in z:
+     print(item)
---
+++ learning file/18_python_learning day 15.py
@@ -0,0 +1,23 @@
+# for i in range(9):
+#      print(i)
+# i = int(input("Enter the number"))#keep i is 0
+# while (i < 3):
+#     print(i)
+#   i=i+1
+# count = 5
+# while (count > 0 ):
+#     print(count)
+#     count=count-1
+
+# while (i<38):
+#     i = int(input("Enter the number: "))
+    
+#     print(i)
+    
+# print("done with loop")    
+x = 5
+while ( x>0 ):
+    print(x)
+    x = x-1
+else:
+    print("good day")
